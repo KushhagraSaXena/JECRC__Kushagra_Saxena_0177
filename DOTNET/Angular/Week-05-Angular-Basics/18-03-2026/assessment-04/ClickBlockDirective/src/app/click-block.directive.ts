de14565d84b999db6f60fc2c86f4cc4800353020
kushaProject <Kusa@email.com>
2026-03-18 14:27:51 +0530

    Assessments

--- DOTNET/Angular/Week-05-Angular-Basics/18-03-2026/assessment-04/ClickBlockDirective/src/app/click-block.directive.ts
+++ DOTNET/Angular/Week-05-Angular-Basics/18-03-2026/assessment-04/ClickBlockDirective/src/app/click-block.directive.ts
@@ -1,19 +1,28 @@
-import { Directive, Input, HostListener } from '@angular/core';
+import { Directive, Input, HostListener, HostBinding, OnChanges } from '@angular/core';
 
 @Directive({
   selector: '[appClickBlock]',
   standalone: true
 })
-export class ClickBlockDirective {
+export class ClickBlockDirective implements OnChanges {
 
   @Input() appClickBlock: boolean = true;
 
+  @HostBinding('style.opacity') opacity = '1';
+  @HostBinding('style.pointerEvents') pointerEvents = 'auto';
+
+  constructor() {}
+
+  ngOnChanges() {
+    this.opacity = this.appClickBlock ? '1' : '0.5';
+    this.pointerEvents = this.appClickBlock ? 'auto' : 'none';
+  }
+
   @HostListener('click', ['$event'])
   onClick(event: Event) {
-
     if (!this.appClickBlock) {
-      event.preventDefault();      
-      event.stopPropagation();     
+      event.preventDefault();
+      event.stopImmediatePropagation();
       console.log('❌ Click Blocked');
     }
   }
