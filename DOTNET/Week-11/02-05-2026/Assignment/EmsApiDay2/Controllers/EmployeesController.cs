f6752fb60d26d6b4ab231b70cca1c365d0c1ce1c
Kushagra Saxena <kushagrasaxena.cse26@jecrc.ac.in>
2026-05-02 22:43:24 +0530

    Assignment weekly

---
+++ DOTNET/Week-11/02-05-2026/Assignment/EmsApiDay2/Controllers/EmployeesController.cs
@@ -0,0 +1,34 @@
+﻿using EmsApiDay2.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmsApiDay2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public EmployeesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Employees
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Employee>>> GetEmployees()
+        {
+            return await _context.Employees.ToListAsync();
+        }
+
+        // POST: api/Employees
+        [HttpPost]
+        public async Task<ActionResult<Employee>> PostEmployee(Employee employee)
+        {
+            _context.Employees.Add(employee);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetEmployees), new { id = employee.Id }, employee);
+        }
+    }
+}
