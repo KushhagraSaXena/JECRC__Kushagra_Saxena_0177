8d448122d03e18ab9b5ff17234a47878a0eb9bc8
Kushagra Saxena <kushagrasaxena.cse26@jecrc.ac.in>
2026-05-05 11:32:54 +0530

    CICD and Azure

---
+++ DOTNET/Week-11/05-05-2026/Case Study/ProjectCICD/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProjectCICD.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
